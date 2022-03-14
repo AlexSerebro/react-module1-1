f5ac34aa54b88984de969a20d2080507d641e514
Aleksandr Serebro <aleksandrserebroz@gmail.com>
2022-03-14 21:09:58 +0200

    add style

---
+++ src/components/ColorPicker/ColorPicker.js
@@ -0,0 +1,19 @@
+import './ColorPicker.css';
+
+function ColorPicker({ options }) {
+  return (
+    <div className="ColorPicker">
+      <h2 className="ColorPicker__title">Color Picker</h2>
+      <div>
+        {options.map(option => (
+          <span
+            className="ColorPicker__option"
+            style={{ backgroundColor: option.color }}
+          ></span>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default ColorPicker;
